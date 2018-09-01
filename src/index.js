b30324587709053bd7f08b1f8458610d63040bfc
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-09-01 19:17:06 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -8,15 +8,54 @@ const BUTCHER_PRODUCTS = [
 // Define these exported classes
 export class OlderCoaster extends React.Component {
   // your code here
+  
+  render() {
+    return (
+      <div className="oldercoaster">
+        <p>Two grannies having the time of their life!</p>
+        <p>Passengers:</p>
+        <ul>
+          <li>Agnes</li>
+          <li>Muriel</li>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export class InFrontOfYou extends React.Component {
   // your code here
+  render(){
+    return (render() {
+    return (
+      <div className="oldercoaster">
+        <p>Two grannies having the time of their life!</p>
+        <p>Passengers:</p>
+        <ul>
+          <li>Agnes</li>
+          <li>Muriel</li>
+        </ul>
+      </div>
+    )
+  })
+  }
+ 
 }
 
 export class ButcherShop extends React.Component {
   // your code here
+  const ROTTEN_MEAT_PRODUCTS = ["Old Fish", "Sweeney Todd", "Tomatoes?"]
+ 
+  const products = [<li>{ROTTEN_MEAT_PRODUCTS[0]}</li>, <li>ROTTEN_MEAT_PRODUCTS[1]</li>, etc...]
+   render() {
+      return (
+       <div>
+          { products }
+       </div>
+     )
+   }
 }
+
 
 
 ReactDOM.render(
